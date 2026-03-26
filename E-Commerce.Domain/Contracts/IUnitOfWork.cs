d756076c7181fe4c78a8e37cee3b20c52421c75b
AmrAlaraby <amr2amr3@gmail.com>
2026-03-26 07:32:19 +0200

    Add generic repository and unit of work patterns
    Introduced IGenaricRrepository and IUnitOfWork interfaces to abstract data access and persistence logic. Implemented GenaricRepository and UnitOfWork classes using Entity Framework Core, enabling generic CRUD operations and centralized transaction management. This refactor improves maintainability and testability of the data access layer.

---
+++ E-Commerce.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync();
+        IGenaricRrepository<TEntity, TKey> GetRrepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+    }
+}
---
+++ E-Commerce.Persistence/Repositories/GenaricRepository.cs
@@ -0,0 +1,46 @@
+﻿using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities;
+using E_Commerce.Persistence.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Repositories
+{
+    public class GenaricRepository<TEntity, TKey> : IGenaricRrepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _storeDbContext;
+
+        public GenaricRepository(StoreDbContext storeDbContext) 
+        {
+            this._storeDbContext = storeDbContext;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _storeDbContext.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _storeDbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        {
+            return await _storeDbContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _storeDbContext.Set<TEntity>().Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _storeDbContext.Set<TEntity>().Update(entity);
+        }
+    }
+}
