a88c322e94f0e914cbe69c5388b97875ba3e17d7
AmrAlaraby <amr2amr3@gmail.com>
2026-07-06 16:28:24 +0300

    Add common result and error handling classes
    Introduced E_Commerce.Shared.CommonResult namespace with Error, ErrorType, and Result classes. Provides structured error representation, error types, and generic result handling for success and failure scenarios.

---
+++ E-Commerce.Shared/CommonResult/Error.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.CommonResult
+{
+    public class Error
+    {
+
+        public string Code { get; }
+        public string Description { get; }
+        public ErrorType Type  { get; }
+        private Error(string code, string description, ErrorType type)
+        {
+            Code = code;
+            Description = description;
+            Type = type;
+        }
+        public static Error Failure (string code = "General.Failure", string description = "A General Falure Has Occured")
+        {
+            return new Error(code, description, ErrorType.Failure);
+        }
+        public static Error Validation(string code = "General.Validation", string description = "Validation Error Has Occured")
+        {
+            return new Error(code, description , ErrorType.Validation);
+        }
+        public static Error NotFound(string code = "General.NotFound", string description = "The Requested Resorce Was Not Found")
+        {
+            return new Error(code , description , ErrorType.NotFound);
+        }
+        public static Error UnAuthorized(string code = "General.UnAuthorized", string description = "You Are Not Authorized To This EndPoint")
+        {
+            return new Error(code , description , ErrorType.UnAuthorized);
+        }
+        public static Error Forbidden(string code = "General.Forbidden", string description = "You Do Not Have Permission To Access That Resouurse")
+        {
+            return new Error(code , description , ErrorType.Forbidden);
+        }
+        public static Error InvalidCredentials(string code = "General.InvalidCredentials", string description = "The provided Credentials are Invalid")
+        {
+            return new Error(code , description , ErrorType.InvalidCredentials);
+        }
+    }
+}
---
+++ E-Commerce.Shared/CommonResult/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Shared.CommonResult
+{
+    public class Result
+    {
+        protected readonly List<Error> _errors = [];
+        public bool IsSuccess => _errors.Count ==0;
+        public bool IsFailure => !IsSuccess;
+        public IReadOnlyList<Error> Errors => _errors;
+        protected Result()
+        {
+            
+        }
+        protected Result(Error error)
+        {
+            _errors.Add(error);
+        }
+        protected Result(List<Error> errors)
+        {
+            _errors.AddRange(errors);
+        }
+
+        public static Result Ok() => new Result();
+        public static Result Faill(Error error) => new Result(error);
+        public static Result Faill(List<Error> errors) => new Result(errors);
+
+
+
+    }
+    public class Result<TValue>:Result
+    {
+        private readonly TValue _value;
+        public TValue Value => IsSuccess ? _value : throw new InvalidOperationException("Can not access the value of Failed Result");
+
+        public Result(TValue value):base()
+        {
+            _value = value;
+        }
+        public Result(Error error):base(error)
+        {
+            _value = default!;
+        }
+        public Result(List<Error> errors) : base(errors)
+        {
+            _value = default!;
+        }
+        public static Result<TValue> Ok(TValue value) => new (value);
+        public static new Result<TValue> Faill(Error error) => new (error);
+        public static new Result<TValue> Faill(List<Error> errors) => new (errors);
+
+
+    }
+
+}
